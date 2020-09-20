e6b21f054e1c973725586f722d5a71d1eeff6a12
Lily <lilyau0112@gmail.com>
2020-09-21 02:32:06 +0800

    build and style jumbotron

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+import Home from './pages/Home'
 
 export function App() {
   return (
@@ -16,7 +18,7 @@ export function App() {
                 <p>I will be the browse page</p>
             </Route>
             <Route path="/">
-                <p>I am going to be a cloned Netflix application</p>
+                <Home />
             </Route>
         </Switch>
     </Router>
---
+++ src/containers/jumbotron.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import jumboData from '../fixtures/jumbo.json'
+import Jumbotron from '../components/jumbotron'
+
+function JunbotronContainer(){
+    return(
+        <Jumbotron.Container>
+        {jumboData.map(item => (
+            <Jumbotron key={item.id} direction={item.direction}>
+                <Jumbotron.Pane>
+                    <Jumbotron.Title>{item.title}</Jumbotron.Title>
+                    <Jumbotron.Subtitle>{item.subTitle}</Jumbotron.Subtitle>
+                </Jumbotron.Pane>
+                <Jumbotron.Pane>
+                    <Jumbotron.Image src={item.image} alt={item.alt}></Jumbotron.Image>
+                </Jumbotron.Pane>
+                
+            </Jumbotron>
+        ))}
+        
+        </Jumbotron.Container>
+    )
+    
+}
+
+export default JunbotronContainer
