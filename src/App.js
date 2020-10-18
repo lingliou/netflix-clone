26aa292e40b7a633c235810878818ebf05e77661
Lily <lilyau0112@gmail.com>
2020-10-18 16:07:19 +0800

    protected route v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
-import { Route } from 'react-router-dom'
 import * as ROUTES from './constants/routes'
 import { IsUserRedirect, ProtectedRout } from './helpers/routes'
 
