e78edb7139ce21462ba7ed73e5bbf2d574d85071
Lily <lilyau0112@gmail.com>
2020-10-19 18:22:38 +0800

    data selectionMap

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { BrowseContainer } from '../containers/browse'
 import { useContent } from '../hooks'
+import { selectionMap } from '../utils'
 
 export default function Browse(){
     const {series} = useContent('series')
     const {films} = useContent('films')
 
+    const slides = selectionMap( {series, films})
+
+    console.log(slides)
 
     return(
-        <BrowseContainer/>
+        <BrowseContainer slides={slides}/>
     )
 }
